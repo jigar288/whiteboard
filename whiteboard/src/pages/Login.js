b2e471ce0d844b0f87e2c1f5b6e6d0acb5984907
jmanusig1 <jmanus2@uic.edu>
2020-11-22 15:29:11 -0600

    Basic Login

--- whiteboard/src/pages/Login.js
+++ whiteboard/src/pages/Login.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import "../css/Login.css";
+import {PrimaryButton, Stack, DefaultPalette, TextField, MaskedTextField, Label} from 'office-ui-fabric-react';
 
 export default function Login() {
   const [email, setEmail] = useState("");
@@ -15,8 +16,27 @@ export default function Login() {
     event.preventDefault();
   }
 
+  function _alertClicked(){
+    alert('Clicked');
+  }
+
+  const columnProps = {
+    tokens: { childrenGap: 15 },
+    styles: { root: { width: 300 } },
+  };
+
   return (
     <div className="Login">
+
+      <Stack {...columnProps}>
+        <Label>Welcome To Whiteboard</Label>
+        <Label>Login</Label>
+        <TextField label="Email" placeholder="Enter Email Here" />
+        <TextField label="Password" placeholder="Enter Password Here" type="password" canRevealPassword />
+        <PrimaryButton text="Login" onClick={_alertClicked} allowDisabledFocus/>
+      </Stack>
+
+        {/*
       <Form onSubmit={handleSubmit}>
         <Form.Group size="lg" controlId="email">
           <Form.Label>Email</Form.Label>
@@ -35,10 +55,12 @@ export default function Login() {
             onChange={(e) => setPassword(e.target.value)}
           />
         </Form.Group>
+        <PrimaryButton text="Login" onClick={_alertClicked} allowDisabledFocus/>
         <Button block size="lg" type="submit" disabled={!validateForm()}>
           Login
         </Button>
       </Form>
+      */}
     </div>
   );
 }
