1e55f4fc7c1834cf47509d2df186017f73425be7
Arpita-Kumari <49378845+Arpita-Kumari@users.noreply.github.com>
2020-11-23 17:53:08 -0600

    updated with pictures of the handout

--- whiteboard/src/data/grades/EnglishGradeData.js
+++ whiteboard/src/data/grades/EnglishGradeData.js
@@ -7,29 +7,34 @@ export const EnglishGradeData = [
         type: 'Homework',
         grade: 100,
         details: '/link-to-details-page',
+        imageHandout: 'https://images-ext-2.discordapp.net/external/6wd3KJB-bWhuGbaGxLjMuSVsHNeozL4bd5HWndoHGB0/https/i.imgur.com/vEovbvq.png',
     },
     {
         assignmentName: 'Quiz 4: Green Eggs and Ham',
         type: 'Quiz',
         grade: 85,
         details: '/link-to-details-page',
+        imageHandout: 'https://images-ext-2.discordapp.net/external/jleLqA4qU3evwrcAfpmjyjiUfgZ0IbD_0ezrtxE3Ohw/https/i.imgur.com/vEovbvqh.jpg',
     }, 
     {
         assignmentName: 'Exam 1: Essay revisions',
         type: 'Exam',
         grade: 80,
         details: '/link-to-details-page',
+        imageHandout: 'https://images-ext-2.discordapp.net/external/4TG_sSRJi8-NPDNIY-ySxEkwNjoXv1aBJa4xvnOq0DA/https/i.imgur.com/1Nxt6Rs.png',
     },
     {
         assignmentName: 'Quiz 3: Book Reading',
         type: 'Quiz',
         grade: 75,
         details: '/link-to-details-page',
+        imageHandout: 'https://images-ext-2.discordapp.net/external/6wd3KJB-bWhuGbaGxLjMuSVsHNeozL4bd5HWndoHGB0/https/i.imgur.com/vEovbvq.png',
     }, 
     {
         assignmentName: 'Quiz 2: Book quiz',
         type: 'Quiz',
         grade: 95,
         details: '/link-to-details-page',
+        imageHandout: 'https://images-ext-1.discordapp.net/external/H0TRA2g-hCdydD9L1Qn40l-C1-1_gvR1yBFE1MlhuzY/https/i.imgur.com/rzEEOcU.png',
     }     
 ]
