9828c611fd360a7ae6f84e71148ae2a6a18d168c
Arpita-Kumari <49378845+Arpita-Kumari@users.noreply.github.com>
2020-11-23 17:22:35 -0600

    image of the handout added for each assignment

--- whiteboard/src/data/grades/ReadingGradeData.js
+++ whiteboard/src/data/grades/ReadingGradeData.js
@@ -4,6 +4,7 @@ export const ReadingGradeData = [
         type: 'Homework',
         grade: 100,
         details: '/link-to-details-page',
+        imageHandout: 'https://drive.google.com/file/d/1ap8h-4pUc3p_NT5IL8HyvnEr4Xb1oEMc/view?usp=sharing'
     },
   
     {
@@ -11,17 +12,20 @@ export const ReadingGradeData = [
         type: 'Quiz',
         grade: 85,
         details: '/link-to-details-page',
+        imageHandout: 'https://drive.google.com/file/d/12OQuiTwPfFYkAIWmAL4eGGd4Wxtc0KSd/view?usp=sharing'
     }, 
     {
         assignmentName: 'Exam 1: Vocabulary',
         type: 'Exam',
         grade: 80,
         details: '/link-to-details-page',
+        imageHandout: 'https://drive.google.com/file/d/16uNhnF7-fMRVaHkGrWdJKC13X4Cr2ORe/view?usp=sharing'
     },
     {
         assignmentName: 'Quiz 3: Conjunctions',
         type: 'Quiz',
         grade: 75,
         details: '/link-to-details-page',
+        imageHandout: 'https://drive.google.com/file/d/1UASO7FhDvYWnmTeimN6C7R3ndpFUx6qP/view?usp=sharing',
     }     
 ]
