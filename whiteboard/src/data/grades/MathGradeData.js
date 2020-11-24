eb6931da9b714ce191b5b426a46805047f511a26
Arpita-Kumari <49378845+Arpita-Kumari@users.noreply.github.com>
2020-11-23 18:09:10 -0600

    updated with pictures of the handout

--- whiteboard/src/data/grades/MathGradeData.js
+++ whiteboard/src/data/grades/MathGradeData.js
@@ -7,29 +7,34 @@ export const MathGradeData = [
         type: 'Homework',
         grade: 100,
         details: '/link-to-details-page',
+        imageHandout: 'https://i.imgur.com/6YWb7aW.png',
     },
     {
         assignmentName: 'Quiz 1: Triangles',
         type: 'Quiz',
         grade: 85,
         details: '/link-to-details-page',
+        imageHandout:'https://i.imgur.com/4fVEmq9.png',
     },
     {
         assignmentName: 'Quiz 2: Subtraction',
         type: 'Quiz',
         grade: 75,
         details: '/link-to-details-page',
+        imageHandout: 'https://media.discordapp.net/attachments/769001450310336542/780565906979684372/unknown.png',
     }, 
     {
         assignmentName: 'Quiz 3: Addition',
         type: 'Quiz',
         grade: 95,
         details: '/link-to-details-page',
+        imageHandout: 'https://cdn.discordapp.com/attachments/769001450310336542/780566037673279498/unknown.png',
     },
     {
         assignmentName: 'Exam 1: Addition and Subtraction',
         type: 'Exam',
         grade: 80,
         details: '/link-to-details-page',
+        imageHandout: 'https://media.discordapp.net/attachments/769001450310336542/780566213557354506/unknown.png',
     }     
 ]
