4737cd8db877b62e7336e042b05dc1bffcb5c283
jigarpatel <jigar.moyo@gmail.com>
2020-11-23 23:11:48 -0600

    added help page. partial credit to michael

--- whiteboard/src/components/NavigationBar.js
+++ whiteboard/src/components/NavigationBar.js
@@ -69,7 +69,7 @@ class NavigationBar extends React.Component {
                                 <Nav.Link href='/submit-assignments' onClick={(e) => this.shouldDisable('/submit-assignments', e)}>Submit Assignment</Nav.Link>
                             </Nav.Item>
                             <Nav.Item>
-                                <Nav.Link href='/' onClick={(e) => this.shouldDisable('/', e)}>Help</Nav.Link>
+                                <Nav.Link href='/help' onClick={(e) => this.shouldDisable('/help', e)}>Help</Nav.Link>
                             </Nav.Item>                            
                             <Nav.Item>
                                 <Nav.Link href='/home-login' onClick={(e) => this.shouldDisable('/', e)}>Logout</Nav.Link>
